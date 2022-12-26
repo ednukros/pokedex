b8b55f334892adbefe0c1020a3b7cf1ba21cd737
TamaraGlez <tamara.gonzalez@bootcamp-upgrade.com>
2022-12-26 20:03:06 +0100

    feat: create-draw

--- main.js
+++ main.js
@@ -13,3 +13,27 @@ function itemFetch (number){
     }
 }
 itemFetch(10);
+
+function drawPokemon() {
+    const h2$$ = document.createElement('h2');
+    const imageContainer$$ = document.createElement('div');
+    const img$$ = document.createElement('img');
+    const id$$ = document.createElement('h3');
+    const types$$ = document.createElement('div');
+    const nameType$$ = document.createElement('span');
+
+    imageContainer$$.setAttribute("class","imageContainer");
+    types$$.setAttribute("class","element");
+    nameType$$.setAttribute("class","nameType");
+
+    card$$.appendChild(h2$$);
+    card$$.appendChild(imageContainer$$);
+    imageContainer$$.appendChild(img$$);
+    card$$.appendChild(id$$);
+    card$$.appendChild(types$$);
+    types$$.appendChild(nameType$$);  
+
+    h2$$.textContent = pokemons
+    
+}
+drawPokemon();
