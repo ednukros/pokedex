70f6dea7d4854a6991f107ad23bd53a9ce75178b
TamaraGlez <tamara.gonzalez@bootcamp-upgrade.com>
2023-01-02 19:21:32 +0100

    fix code

--- main.js
+++ main.js
@@ -12,25 +12,6 @@ const next = document.querySelector("#next");
 const ol$$ = document.querySelector("#pokedex");
 const container$$ = document.querySelector(".container");
 
-const typeColors = {
-  electric: "#FFEA70",
-  normal: "#B09398",
-  fire: "#FF675C",
-  water: "#0596C7",
-  ice: "#AFEAFD",
-  rock: "#999799",
-  flying: "#7AE7C7",
-  grass: "#4A9681",
-  psychic: "#FFC6D9",
-  ghost: "#561D25",
-  bug: "#A2FAA3",
-  poison: "#795663",
-  ground: "#D2B074",
-  dragon: "#DA627D",
-  steel: "#1D8A99",
-  fighting: "#2F2F2F",
-  default: "",
-};
 
 const getPokemons = async () => {
   response = await fetch(
@@ -79,7 +60,7 @@ function renderFront(resul) {
     nameType$$.textContent = tipo.type.name + " ";
 
     if (tipo.type.name === "grass") {
-        
+
         nameType$$.style.background = "#4A9681"
 
       }
